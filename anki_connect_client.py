3c955ea84d47f7f1d95576e786110193a4900117
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2018-12-03 14:01:25 -0700

    Add notesInfo method docs

--- anki_connect_client.py
+++ anki_connect_client.py
@@ -883,7 +883,30 @@ class AnkiClient(AnkiClientBase):
 
   def notesInfo(self, notes):
     """
-    TODO
+    Returns a list of objects containing for each note ID the note fields, tags, note type and the cards belonging to the note.
+
+    Sample request:
+
+      client.notesInfo(
+        notes = [1502298033753]
+      )
+
+    Sample result:
+
+      {
+        "result": [
+          {
+            "noteId":1502298033753,
+            "modelName": "Basic",
+            "tags":["tag","another_tag"],
+            "fields": {
+              "Front": {"value": "front content", "order": 0},
+              "Back": {"value": "back content", "order": 1}
+            }
+          }
+        ],
+        "error": null
+      }
     """
     wr, r = self.send_as_json(action = "notesInfo", params = dict(
       notes = notes
