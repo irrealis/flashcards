723914c592a6a8ce202b1fbdabf05016da67b56a
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-13 17:04:21 -0600

    Add SVEC-11.07 and CET-14.07.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/examples_14_7.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "14",
+  chapter_title = "Partial Derivatives",
+
+  section_num = "07",
+  section_title = "Minimum and Maximum Values",
+
+  problem_type = "Example",
+  problem_abbrev = "Xm",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Examples_header.yml",
+  note_file = "programmatic/Example_note.yml",
+  output_file = "14.07.Examples.yml",
+)
+
+generate_yml(variables, 7)
