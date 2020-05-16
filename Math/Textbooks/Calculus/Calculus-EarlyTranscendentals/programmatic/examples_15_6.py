4162ec97ba37b64107700921efece33674e18712
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-16 16:15:15 -0600

    Add SVEC-12.05 and CET-15.06.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/examples_15_6.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "15",
+  chapter_title = "Multiple Integrals",
+
+  section_num = "06",
+  section_title = "Triple Integrals",
+
+  problem_type = "Example",
+  problem_abbrev = "Xm",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Examples_header.yml",
+  note_file = "programmatic/Example_note.yml",
+  output_file = "15.06.Examples.yml",
+)
+
+generate_yml(variables, 6)
