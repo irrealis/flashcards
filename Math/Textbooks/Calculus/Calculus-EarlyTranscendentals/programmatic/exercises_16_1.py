41b982bc976125e029a5fa4d012e5d484e14a5c7
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-18 18:58:35 -0600

    Add SVEC-13.01 and CET-16.01.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/exercises_16_1.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "16",
+  chapter_title = "Vector Calculus",
+
+  section_num = "01",
+  section_title = "Vector Fields",
+
+  problem_type = "Exercise",
+  problem_abbrev = "Xs",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Exercises_header.yml",
+  note_file = "programmatic/Exercise_note.yml",
+  output_file = "16.01.Exercises.yml",
+)
+
+generate_yml(variables, 36)
