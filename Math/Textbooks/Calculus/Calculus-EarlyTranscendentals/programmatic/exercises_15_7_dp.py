cc5c982c90b88f1c20b24411b80fa9a6e82e6546
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-18 18:01:07 -0600

    Add CET-15 review, problems-plus.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/exercises_15_7_dp.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "15",
+  chapter_title = "Multiple Integrals",
+
+  section_num = "07-DP",
+  section_title = "Discovery Project - The Intersection of Three Cylinders",
+
+  problem_type = "Exercise",
+  problem_abbrev = "Xs",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Exercises_header.yml",
+  note_file = "programmatic/Exercise_note.yml",
+  output_file = "15.07-DP.Exercises.yml",
+)
+
+generate_yml(variables, 5)
