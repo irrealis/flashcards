30a10cc99fc3a5a29aebe1962833c958a45c918e
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-21 17:59:40 -0600

    Add SVEC-13.05 and CET-16.05.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/examples_16_5.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "16",
+  chapter_title = "Vector Calculus",
+
+  section_num = "05",
+  section_title = "Curl and Divergence",
+
+  problem_type = "Example",
+  problem_abbrev = "Xm",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Examples_header.yml",
+  note_file = "programmatic/Example_note.yml",
+  output_file = "16.05.Examples.yml",
+)
+
+generate_yml(variables, 5)
