5492044161818e979d709d068446a37375f91aba
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-26 18:02:38 -0600

    Add SVEC-13.08 and CET-16.08.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/examples_16_8.py
@@ -0,0 +1,28 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "16",
+  chapter_title = "Vector Calculus",
+
+  section_num = "08",
+  section_title = "Stoke's Theorem",
+  section_tag = "CET-16.08-StokesTheorem",
+
+  problem_type = "Example",
+  problem_abbrev = "Xm",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Examples_header.yml",
+  note_file = "programmatic/Example_note.yml",
+  output_file = "16.08.Examples.yml",
+)
+
+generate_yml(variables, 2)
