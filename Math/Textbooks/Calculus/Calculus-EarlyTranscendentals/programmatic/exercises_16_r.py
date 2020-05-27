199e6a1933647b568ee0ce8cc3ceee0d27b197b0
irrealis CHOMP <5480636+irrealis@users.noreply.github.com>
2020-05-27 10:00:11 -0600

    Add CET-16 review and problems-plus exercises.

---
+++ Math/Textbooks/Calculus/Calculus-EarlyTranscendentals/programmatic/exercises_16_r.py
@@ -0,0 +1,27 @@
+from yml_generators import generate_yml
+
+variables = dict(
+  textbook_title = "Calculus - Early Trancendentals",
+  textbook_abbrev = "CET",
+  textbook_tag = "Calculus-EarlyTrancendentals",
+
+  chapter_num = "16",
+  chapter_title = "Vector Calculus",
+
+  section_num = "R-Ex",
+  section_title = "Review",
+
+  problem_type = "Exercise",
+  problem_abbrev = "Xs",
+
+  deck_name = "Staging",
+  model_name = "PWeave",
+  markdown_tab_length = "2",
+  string_templ_delim = "¢",
+
+  header_file = "programmatic/Exercises_header.yml",
+  note_file = "programmatic/Exercise_note.yml",
+  output_file = "16.R-Ex.Exercises.yml",
+)
+
+generate_yml(variables, 41)
